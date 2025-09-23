292a345673b4134769c96a4af31fcb16a84411a2
atikanairaa <atikanaira07@gmail.com>
2025-09-23 14:35:51 +0700

    feat: tambah service rabbitmq

---
+++ rabbitmq/src/test/java/com/naira/rabbitmq/RabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naira.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
